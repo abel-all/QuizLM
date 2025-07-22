b4fdf75f05e1da50794e3e88bf8b1912bae21e69
Abdessamad El Allali <abel-all@e3r7p17.1337.ma>
2025-07-22 20:12:03 +0100

    feat: add dialog to dashboard

--- apps/frontend/app/(website)/_components/CreateNewSection.tsx
+++ apps/frontend/app/(website)/_components/CreateNewSection.tsx
@@ -1,15 +1,18 @@
-import { Button } from '@/components/ui/button'
-import { Plus } from 'lucide-react'
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { DialogTrigger } from "@/components/ui/dialog";
+import { Plus } from "lucide-react";
+import React from "react";
 
 const CreateNewSection = () => {
   return (
-    <div className='w-full flex justify-end'>
+    <div className="w-full flex justify-end">
+      <DialogTrigger asChild>
         <Button className="rounded-full">
-            <Plus /> Create new
+          <Plus /> Create new
         </Button>
+      </DialogTrigger>
     </div>
-  )
-}
+  );
+};
 
-export default CreateNewSection
+export default CreateNewSection;
--- apps/frontend/app/(website)/dashboard/page.tsx
+++ apps/frontend/app/(website)/dashboard/page.tsx
@@ -1,15 +1,33 @@
-import React from 'react'
-import CreateNewSection from '../_components/CreateNewSection'
-import QuizzesSection from '../_components/QuizzesSection'
+import React from "react";
+import CreateNewSection from "../_components/CreateNewSection";
+import QuizzesSection from "../_components/QuizzesSection";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 const Dashboard = () => {
   return (
     <div>
-      <CreateNewSection />
-      <QuizzesSection />
+      <Dialog>
+        <CreateNewSection />
+        <QuizzesSection />
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Add sources</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
+  );
+};
 
-  )
-}
-
-export default Dashboard
+export default Dashboard;
